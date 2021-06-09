b0448d5ce0574cdf70f66ae0d9d941a81086eaab
Darrian Miller <35920220+NinoGotti5@users.noreply.github.com>
2021-06-09 04:52:45 +0000

    Changed answer variable

--- index.js
+++ index.js
@@ -19,14 +19,13 @@ console.log(`Im glad to have applied for ${str} !`);
 
 //b) Modify your to accept user input. Query the user to enter the number of letters that will be relocated.
 
-let info = (Number(input.question("How many letters would you like to relocate? ")));
+let answer = (Number(input.question("How many letters would you like to relocate? ")));
 
-let newEnd2 = str.slice (0,info);
-str.slice(info);
+let newEnd2 = str.slice (0,answer) ;str.slice(answer);
 
 //c) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
-if (info > 10) {
-  console.log(`Your answer, ${info}, is invalid. "Launchcode" only has 10 letters. We defaulted your answer to 3. Using 3 letters, the word ${str} can be converted to ${str.slice(3) + newStr}.`);
+if (answer > 10) {
+  console.log(`Your answer, ${answer}, is invalid. "Launchcode" only has 10 letters. We defaulted your answer to 3. Using 3 letters, the word ${str} can be converted to ${str.slice(3) + newStr}.`);
 } else {
-    console.log(str.slice(info) + newEnd2);
+    console.log(str.slice(answer) + newEnd2);
 }
